979f1e860f70f8d36f7fad27a24e1cbdd09dda44
Stanley Sham <stanley.sham94@gmail.com>
2019-11-13 21:19:07 -0500

    added line to close driver

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-def Skyscanner_scrape(url_link):
+def Skyscanner_scrape():
+    print("Please paste skyscanner link: ")
+    url = input()
     scraped_flights = {}
     profile = webdriver.FirefoxProfile()
     profile.set_preference("browser.cache.disk.enable", False)
@@ -12,10 +14,10 @@ def Skyscanner_scrape(url_link):
     profile.set_preference("browser.cache.offline.enable", False)
     profile.set_preference("network.http.use-cache", False) 
     driver =webdriver.Firefox(profile)
-    driver.get(url_link)
+    driver.get(url)
     print(driver.title)
     assert "Skyscanner" in driver.title # Makes sure that the correct website has been loaded
-    time.sleep(10)
+    time.sleep(20)
     sort = driver.find_element_by_class_name("BpkSelect_bpk-select__32bku")
     sort.click()
     cheap = driver.find_element_by_xpath("//option[@value='CHEAPEST']")
@@ -31,13 +33,14 @@ def Skyscanner_scrape(url_link):
         flight_info = {"origin_dep_time": times[i*4].text, "destination_arr_time": times[i*4+1].text, "destination_dep_time": times[i*4+2].text, "origin_arr_time": times[i*4+3].text, "price": prices[i].text, "link": links[i].get_attribute("href")}
         scraped_flights[i+1] = flight_info
         i += 1
+    driver.close()
     return scraped_flights
 
 
-flights = Skyscanner_scrape("https://www.skyscanner.ca/transport/flights/sfo/tyoa/191118/191125/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&ref=home#/")
+flights = Skyscanner_scrape()
 
 for num, value in flights.items():
     print(num, value)
 
 data = pd.DataFrame.from_dict(flights, orient='index')
-data.to_csv(".data.csv")
+data.to_csv("./data.csv")
