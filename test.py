e9728299bebdb410dbc5772649f804255ccdacc4
Stanley Sham <stanley.sham94@gmail.com>
2019-11-14 19:18:22 -0500

    copied over code from notebook, and tweaked script for user input

--- test.py
+++ test.py
@@ -7,7 +7,9 @@ import time
 def Skyscanner_scrape():
     print("Please paste skyscanner link: ")
     url = input()
-    scraped_flights = {}
+    print("How many flights would you like to scrape?: ")
+    num_flights = input()
+    scraped_flights = []
     profile = webdriver.FirefoxProfile()
     profile.set_preference("browser.cache.disk.enable", False)
     profile.set_preference("browser.cache.memory.enable", False)
@@ -25,22 +27,21 @@ def Skyscanner_scrape():
     time.sleep(1)
     load_more = driver.find_element_by_css_selector("button[class='BpkButton_bpk-button__32HxR BpkButton_bpk-button--secondary__2UhGP']")
     load_more.click()
-    time.sleep(3)
-    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-    time.sleep(3)
-    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
-    times = driver.find_elements_by_css_selector("span[class='BpkText_bpk-text__2NHsO BpkText_bpk-text--base__2vfTl BpkText_bpk-text--bold__4yauk']")
-    # total_time = driver.find_elements_by_css_selector("span[class='BpkText_bpk-text__2NHsO BpkText_bpk-text--sm__345aT Duration_duration__1QA_S]")
     links = driver.find_elements_by_class_name("FlightsTicket_link__kl4DL")
-    prices_list = driver.find_elements_by_css_selector("span[class='BpkText_bpk-text__2NHsO BpkText_bpk-text--lg__3vAKN BpkText_bpk-text--bold__4yauk']")
+    while len(links) < int(num_flights):
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        links = driver.find_elements_by_class_name("FlightsTicket_link__kl4DL")
+        prices_list = driver.find_elements_by_css_selector("span[class='BpkText_bpk-text__2NHsO BpkText_bpk-text--lg__3vAKN BpkText_bpk-text--bold__4yauk']")
+        times = driver.find_elements_by_css_selector("span[class='BpkText_bpk-text__2NHsO BpkText_bpk-text--base__2vfTl BpkText_bpk-text--bold__4yauk']")
+        time.sleep(3)  
     prices = prices_list[3:]
     i = 0
     print(f'Number of times: {len(times)}')
     print(f'Number of links: {len(links)}')
     print(f'Number of prices: {len(prices)}')
-    while i < len(prices):
+    while i < int(num_flights):
         flight_info = {"origin_dep_time": times[i*4].text, "destination_arr_time": times[i*4+1].text, "destination_dep_time": times[i*4+2].text, "origin_arr_time": times[i*4+3].text, "price": prices[i].text, "link": links[i].get_attribute("href")}
-        scraped_flights[i+1] = flight_info
+        scraped_flights.append(flight_info)
         i += 1
     driver.close()
     return scraped_flights
@@ -48,8 +49,16 @@ def Skyscanner_scrape():
 
 flights = Skyscanner_scrape()
 
-for num, value in flights.items():
-    print(num, value)
+flights_data = pd.DataFrame(flights, columns = ['origin_dep_time', 'destination_arr_time', 'destination_dep_time', 'origin_arr_time', 'price', 'link'])
+flights_data.index += 1
 
-data = pd.DataFrame.from_dict(flights, orient='index')
-data.to_csv("./data.csv")
+for i in range(1,flights_data['price'].count()+1):
+    flights_data['price'][i] = flights_data['price'][i].replace('C$', '')
+    flights_data['price'][i] = flights_data['price'][i].replace(',', '')
+flights_data['price'] = flights_data['price'].astype(int)
+
+price_stats = pd.DataFrame(flights_data['price'].describe()).transpose()
+price_stats.head()
+
+flights_data.to_csv("./flights_data.csv")
+price_stats.to_csv("./price_stats.csv")
